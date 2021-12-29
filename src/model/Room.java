2fc46e13230fd40636384a965b024b3d6fa35364
Kota Aoyama <hongcha811@gmail.com>
2021-12-29 15:42:04 +0900

    added Room

---
+++ src/model/Room.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Room implements IRoom {
+
+    public String roomNumber;
+    public Double price;
+    RoomType enumeration;
+
+    @Override
+    public String getRoomNumber() {
+        return null;
+    }
+
+    @Override
+    public Double getRoomPrice() {
+        return null;
+    }
+
+    @Override
+    public RoomType getRoomType() {
+        return null;
+    }
+
+    @Override
+    public boolean isFree() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "roomNumber='" + roomNumber + '\'' +
+                ", price=" + price +
+                ", enumeration=" + enumeration +
+                '}';
+    }
+}
