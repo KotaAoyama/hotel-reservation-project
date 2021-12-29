3f129f271729bb450d111d3f741dee01951b57ef
Kota Aoyama <hongcha811@gmail.com>
2021-12-29 18:23:36 +0900

    updated CustomerService: added collection for saving customers

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -3,17 +3,21 @@ package service;
 import model.Customer;
 
 import java.util.Collection;
+import java.util.Map;
 
 public class CustomerService {
 
+    private static Map<String, Customer> customerMap;
+
     public static void addCustomer(String email, String firstName, String lastName) {
+        customerMap.put(email, new Customer(firstName, lastName, email));
     }
 
     public static Customer getCustomer(String customerEmail) {
-        return null;
+        return customerMap.get(customerEmail);
     }
 
     public static Collection<Customer> getAllCustomers() {
-        return null;
+        return customerMap.values();
     }
 }
