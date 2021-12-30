28ca88c211d3d2026060d4bc41bf99a7f167a459
Kota Aoyama <hongcha811@gmail.com>
2021-12-30 16:09:09 +0900

    updated reserveARoom method in ReservationService

--- src/service/ReservationService.java
+++ src/service/ReservationService.java
@@ -4,32 +4,48 @@ import model.Customer;
 import model.IRoom;
 import model.Reservation;
 
-import java.util.Collection;
-import java.util.Date;
-import java.util.Map;
+import java.util.*;
 
 public class ReservationService {
 
-    private Map<String, Reservation> reservationMap;
+    // key: Customer.email, value: Reservation
+    private Map<String, Collection<Reservation>> emailReservationsMap;
+    // key: roomId???, value: IRoom
+    private Map<String, IRoom> roomIdIRoomMap;
 
-    public static void addRoom(IRoom room) {}
+    public void addRoom(IRoom room) {
+//        roomIdIRoomMap.put(roomId???, room);
+    }
 
-    public static IRoom getARoom(String roomId) {
+    public IRoom getARoom(String roomId) {
+        return roomIdIRoomMap.get(roomId);
+    }
+
+    public Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) {
+
+        Reservation newReservation = new Reservation(customer, room, checkInDate, checkOutDate);
+        Collection<Reservation> resultReservations = new HashSet<>();
+
+        Collection<Reservation> existingReservations = getCustomerReservation(customer);
+        if (existingReservations != null) {
+            existingReservations.add(newReservation);
+            resultReservations = existingReservations;
+        } else {
+            resultReservations.add(newReservation);
+        }
+        emailReservationsMap.put(customer.email, resultReservations);
+
+        return newReservation;
+    }
+
+    public Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) {
         return null;
     }
 
-    public static Reservation reserveARoom(Customer customer, IRoom room, Date checkInDate, Date checkOutDate) {
-        return null;
+    private Collection<Reservation> getCustomerReservation(Customer customer) {
+        return emailReservationsMap.get(customer.email);
     }
 
-    public static Collection<IRoom> findRooms(Date checkInDate, Date checkOutDate) {
-        return null;
-    }
-
-    public static Collection<Reservation> getCustomerReservation(Customer customer) {
-        return null;
-    }
-
-    public static void printAllReservation() {}
+    public void printAllReservation() {}
 
 }
