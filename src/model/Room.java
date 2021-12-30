2f01652627b320105509da7dd9ff3439f3fa68e1
Kota Aoyama <hongcha811@gmail.com>
2021-12-30 12:26:13 +0900

    updated Room

--- src/model/Room.java
+++ src/model/Room.java
@@ -14,22 +14,22 @@ public class Room implements IRoom {
 
     @Override
     public String getRoomNumber() {
-        return null;
+        return roomNumber;
     }
 
     @Override
     public Double getRoomPrice() {
-        return null;
+        return price;
     }
 
     @Override
     public RoomType getRoomType() {
-        return null;
+        return enumeration;
     }
 
     @Override
     public boolean isFree() {
-        return false;
+        return price.equals((double) 0);
     }
 
     @Override
